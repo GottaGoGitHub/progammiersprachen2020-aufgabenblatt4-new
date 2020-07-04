15d85bf3eea797a68d1f8673d88e9b7fe78040fe
fibe8602 <ericgrunitz@gmail.com>
2020-07-04 16:56:02 +0200

    Added task 11

---
+++ source/aufgabe_11.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include <cmath>
+#include <algorithm>
+#include <functional>
+
+bool is_ten(int x) {
+    return (x == 10);
+}
+
+TEST_CASE("add vector values", "[add]") {
+
+    std::vector<int> v_1{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::vector<int> v_2{ 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    std::vector<int> v_3(9);
+
+    std::transform(v_1.cbegin(), v_1.cend(), v_2.cbegin(), v_3.begin(), [](int x, int y) {return x + y;});
+
+    REQUIRE(std::all_of(v_3.begin(), v_3.end(), is_ten));
+}
+
+int main(int argc, char* argv[]) {
+    return Catch::Session().run(argc, argv);
+}
